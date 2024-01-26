328e65fa173ec989d31cb60554ad2f72e73729e3
test <kmj020901@naver.com>
2024-01-26 21:24:46 +0900

    commit

---
+++ src/main/java/com/ohgiraffes/APPLICATION1.java
@@ -0,0 +1,12 @@
+package com.ohgiraffes;
+
+public class APPLICATION1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(1+1);
+        System.out.println("한글");
+
+
+    }
+}
